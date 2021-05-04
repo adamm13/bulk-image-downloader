6c147c15eadb7ff2bf9fbc41dc9a528d68243e2b
Adam Mohammed <13.adamm@gmail.com>
2021-05-04 18:04:02 -0400

    added chrome extension query JS

---
+++ popup/popup.js
@@ -0,0 +1,6 @@
+
+chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, {action: "get_images"}, response => {
+      console.log(response);
+    })
+})
